52f10d123abc29a4ba02a0c3df9bb623733eeeaa
Richard Davis <davisrg@Veridian.local>
2017-04-20 23:44:48 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  Vimmy 2
+//
+//  Created by Richard Davis on 4/17/17.
+//  Copyright (c) 2017 Richard Davis. All rights reserved.
+//  Based on SampleWebView framework (https://github.com/spaceotech/SampleWebView)
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mainMenu: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        mainMenu.reloadData()
+    }
+    
+    //MARK: UITableViewDataSource
+    
+    func numberOfSectionsInTableView(_ tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        configureCell(cell, forRowAtIndexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(_ cell: UITableViewCell, forRowAtIndexPath: IndexPath) {
+        switch forRowAtIndexPath.row {
+        case 0:
+            cell.textLabel?.text = "Moving Around"
+            let cellIcon : UIImage = UIImage(named: "MovingAround")!
+            cell.imageView?.image = cellIcon
+        case 1:
+            cell.textLabel?.text = "Editing"
+            let cellIcon : UIImage = UIImage(named: "Editing")!
+            cell.imageView?.image = cellIcon
+        case 2:
+            cell.textLabel?.text = "Cut, Copy, and Paste"
+            let cellIcon : UIImage = UIImage(named: "CutCopyandPaste")!
+            cell.imageView?.image = cellIcon
+        case 3:
+            cell.textLabel?.text = "Search and Replace"
+            let cellIcon : UIImage = UIImage(named: "SearchandReplace")!
+            cell.imageView?.image = cellIcon
+        case 4:
+            cell.textLabel?.text = "Regular Expressions"
+            let cellIcon : UIImage = UIImage(named: "RegularExpressions")!
+            cell.imageView?.image = cellIcon
+        case 5:
+            cell.textLabel?.text = "Tabs and Windows"
+            let cellIcon : UIImage = UIImage(named: "TabsandWindows")!
+            cell.imageView?.image = cellIcon
+        case 6:
+            cell.textLabel?.text = "Miscellaneous"
+            let cellIcon : UIImage = UIImage(named: "Miscellaneous")!
+            cell.imageView?.image = cellIcon
+        default:
+            break
+        }
+        
+        cell.accessoryType = .disclosureIndicator
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
+        let webViewObj = self.storyboard?.instantiateViewController(withIdentifier: "WebViewControllerID") as! WebViewController
+        
+        switch indexPath.row {
+        case 0:
+            webViewObj.localFile = "MovingAround"
+            webViewObj.section = "Moving Around"
+        case 1:
+            webViewObj.localFile = "Editing"
+            webViewObj.section = "Editing"
+        case 2:
+            webViewObj.localFile = "CutCopyandPaste"
+            webViewObj.section = "Cut, Copy, and Paste"
+        case 3:
+            webViewObj.localFile = "SearchandReplace"
+            webViewObj.section = "Search and Replace"
+        case 4:
+            webViewObj.localFile = "RegularExpressions"
+            webViewObj.section = "Regular Expressions"
+        case 5:
+            webViewObj.localFile = "TabsandWindows"
+            webViewObj.section = "Tabs and Windows"
+        case 6:
+            webViewObj.localFile = "Miscellaneous"
+            webViewObj.section = "Miscellaneous"
+        default:
+            break
+        }
+        
+        self.navigationController?.pushViewController(webViewObj, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAtIndexPath indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    @IBAction func about() {
+        let webViewObj = self.storyboard?.instantiateViewController(withIdentifier: "WebViewControllerID") as! WebViewController
+        
+        webViewObj.localFile = "About"
+        webViewObj.section = "About"
+        
+        self.navigationController?.pushViewController(webViewObj, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ WebViewController.swift
@@ -0,0 +1,62 @@
+//
+//  WebViewController.swift
+//  Vimmy 2
+//
+//  Created by Richard Davis on 4/17/17.
+//  Copyright (c) 2017 Richard Davis. All rights reserved.
+//  Based on SampleWebView framework (https://github.com/spaceotech/SampleWebView)
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKUIDelegate {
+    @IBOutlet weak var viewContent: WKWebView!
+    @IBOutlet weak var sectionTitle: UILabel!
+    
+    var localFile = ""
+    var section = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set section title
+        sectionTitle.text = section
+        
+        // Load local content
+        let localFilePath = Bundle.main.url(forResource: localFile, withExtension: "html", subdirectory: "Content")
+        let localRequest = URLRequest(url: localFilePath!)
+        viewContent.load(localRequest as URLRequest)
+    }
+    
+    //MARK: WebView Delegate Methods
+    
+    func webViewDidStartLoad(_ webView: WKWebView) {
+    }
+    
+    func webViewDidFinishLoad(_ webView: WKWebView) {
+    }
+    
+    func webView(_ webView: WKWebView, didFailLoadWithError error: Error) {
+    }
+    
+    @IBAction func actionBack(_ sender: AnyObject) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
